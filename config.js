ecc8d2194d3227a1eb922d3653e5682b50411d04
evdrn <jasper.teo.hy@gmail.com>
2025-04-27 23:14:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 export const CONFIG = {
-    apiKey: "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",
+    apiKey: "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",
     apiUrl: "https://api.openai.com/v1/chat/completions",
     model: "gpt-3.5-turbo"
   };
